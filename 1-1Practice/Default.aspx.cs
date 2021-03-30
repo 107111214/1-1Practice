6cf11f9fd52a12860f93e8d894b23d1e86e43810
思甯 王 <107111214@mail.oit.edu.tw>
2021-03-30 16:09:00 +0800

    雙精準度3.141

--- 1-1Practice/Default.aspx.cs
+++ 1-1Practice/Default.aspx.cs
@@ -8,7 +8,8 @@ using System.Web.UI.WebControls;
 namespace _1_1Practice {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-            mt_GetString();
+            double num = Math.Round(3.1415926, 3);
+            Response.Write(num);
         }
     }
 }
